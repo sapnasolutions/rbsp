cb390b28cf670f0d4edec6492664cba57d6273f3
harishankar <hari@sapnasolutions.com>
2010-04-13 19:23:49 +0530

    CSRF first cut

--- config/initializers/cross_site_request_forgery.rb
+++ config/initializers/cross_site_request_forgery.rb
@@ -6,33 +6,22 @@ module RailsBestPractices
   module Checks
     
     class CrossSiteRequestForgery < Check
-      
       def interesting_nodes
-        [:call]
+        [:class]
       end
       
       def interesting_files
-        /app\/controllers\/application_controller.rb/
+        CONTROLLER_FILES
       end
       
       def evaluate_start(node)
-        @no_forgery = true
-        $log.info "------------------------------"
-        $log.debug node.inspect
-        $log.debug node.class
-        $log.debug node.message
-        $log.debug node.body
-        $log.debug node.file
-        $log.debug node.node_type
-        $log.info "------------------------------"
-        
-        if node.message == :protect_from_forgery
-          @no_forgery = false
-        end
-        add_error "Cross Site Request Forgery (CSRF)" if @no_forgery
+        add_error "Cross Site Request Forgery (CSRF)" unless csrf?(node)
       end
-      
-      
+
+      private
+      def csrf?(node)
+        node.grep_nodes({:node_type => :call}).collect{|i| i[2]}.include? :protect_from_forgery
+      end
     end
   end
 end
