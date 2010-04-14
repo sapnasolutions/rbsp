a72af5e549d463516bffa1bebd3b555c7f21393c
harishankar <hari@sapnasolutions.com>
2010-04-14 17:57:57 +0530

    First cut for mass assignment. It currently checks all initializer files for ActiveRecord::Base.send(:attr_accesible, nil)

--- config/initializers/mass_assignment.rb
+++ config/initializers/mass_assignment.rb
@@ -9,22 +9,31 @@ module RailsBestPractices
       INITIALIZER_FILES = /config\/initializers\/.*rb/
 
       def interesting_nodes
-        [:attrasgn]
+        [:call]
       end
       
       def interesting_files
         INITIALIZER_FILES
       end
       
+      def initialize
+        super
+      end
+
       def evaluate_start(node)
-        add_error "Mass assignment vulnerability. Visit: http://guides.rubyonrails.org/security.html#mass-assignment" unless mass_assignment_proof?(node)
+        add_error "Mass assignment vulnerability. Please check: http://guides.rubyonrails.org/security.html#mass-assignment" unless mass_assignment_proof?(node)
       end
 
       private
       def mass_assignment_proof?(node)
-        $log.debug("In mass assignment proof : #{node.file}")
+        if(node.message == :send)
+          cn = node.grep_nodes({:node_type => :lit}).collect{|i| i[1]}
+          cn.include? :attr_accessible
+        else
+          true
+        end
       end
-      
+
     end
   end
 end
