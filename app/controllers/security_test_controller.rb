b26d0b3da274c1b4ad6f7a9cadc9d01d5ede57e3
Ranjith <ranjith@rans.local>
2010-04-19 15:54:24 +0530

    Filter certain request parameters(like password, credit card numbers,... etc) from your log.

--- app/controllers/security_test_controller.rb
+++ app/controllers/security_test_controller.rb
@@ -1,5 +1,8 @@
 class SecurityTestController < ApplicationController
+  
   skip_before_filter :verify_authenticity_token
+  filter_parameter_logging :password
+  
   def index
     @rbp_run_hash = RbpCommons.run
   end
