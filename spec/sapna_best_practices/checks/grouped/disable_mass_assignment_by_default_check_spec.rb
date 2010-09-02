22bcba2ee486fe9287b50c0d93006700524d1842
Rien Croonenborghs <rien@willow.local>
2010-09-02 13:23:45 +0530

    updated the test output

--- spec/sapna_best_practices/checks/grouped/disable_mass_assignment_by_default_check_spec.rb
+++ spec/sapna_best_practices/checks/grouped/disable_mass_assignment_by_default_check_spec.rb
@@ -5,7 +5,7 @@ describe SapnaBestPractices::Checks::Grouped::DisableMassAssignmentByDefaultChec
     @runner = init_grouped_runner(SapnaBestPractices::Checks::Grouped::DisableMassAssignmentByDefaultCheck.new)
   end
   
-  it "should warn of attr_protected" do
+  it "should warn of disabled mass assignment" do
     file_hashes = [
       {:filename => "config/initializers/foo.rb", :content => ""},
       {:filename => "config/initializers/bar.rb", :content => ""},
@@ -14,7 +14,7 @@ describe SapnaBestPractices::Checks::Grouped::DisableMassAssignmentByDefaultChec
     run_grouped_and_check_for_error(@runner, file_hashes, ": - mass assignment is not turned off by default (initialize it)")
   end
   
-  it "should not warn of attr_protected" do
+  it "should not warn of disabled mass assignment" do
     file_hashes = [
       {:filename => "config/initializers/foo.rb", :content => "ActiveRecord::Base.send(:attr_accessible, nil)"},
       {:filename => "config/initializers/bar.rb", :content => ""},
