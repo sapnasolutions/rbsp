f91da9c7a4efa7bb128c13d29e94c6e0148914be
Rien Croonenborghs <rien@willow.local>
2010-09-02 13:23:09 +0530

    added rspec test for checks on disabled mass assignment by default

---
+++ spec/sapna_best_practices/checks/grouped/disable_mass_assignment_by_default_check_spec.rb
@@ -0,0 +1,26 @@
+require File.join(File.dirname(__FILE__) + '/../../../spec_helper')
+
+describe SapnaBestPractices::Checks::Grouped::DisableMassAssignmentByDefaultCheck do
+  before(:each) do
+    @runner = init_grouped_runner(SapnaBestPractices::Checks::Grouped::DisableMassAssignmentByDefaultCheck.new)
+  end
+  
+  it "should warn of attr_protected" do
+    file_hashes = [
+      {:filename => "config/initializers/foo.rb", :content => ""},
+      {:filename => "config/initializers/bar.rb", :content => ""},
+      {:filename => "config/initializers/baz.rb", :content => ""}
+    ]    
+    run_grouped_and_check_for_error(@runner, file_hashes, ": - mass assignment is not turned off by default (initialize it)")
+  end
+  
+  it "should not warn of attr_protected" do
+    file_hashes = [
+      {:filename => "config/initializers/foo.rb", :content => "ActiveRecord::Base.send(:attr_accessible, nil)"},
+      {:filename => "config/initializers/bar.rb", :content => ""},
+      {:filename => "config/initializers/baz.rb", :content => ""}
+    ]    
+    run_grouped_and_check_for_no_error(@runner, file_hashes)
+  end
+  
+end
