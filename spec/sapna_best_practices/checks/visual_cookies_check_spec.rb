3014758ca71a1038a6933a2b6499d797275e027e
Rien Croonenborghs <rien@willow.local>
2010-09-02 12:12:13 +0530

    added rspec test for visual check on cookies

---
+++ spec/sapna_best_practices/checks/visual_cookies_check_spec.rb
@@ -0,0 +1,67 @@
+require File.join(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe SapnaBestPractices::Checks::VisualCookiesCheck do
+  before(:each) do
+    @runner = init_single_runner(SapnaBestPractices::Checks::VisualCookiesCheck.new)
+  end
+  
+  it "should warn of cookies in application controller" do
+    content = <<-EOF
+    class ApplicationController < ActionController::Base
+      def bar
+        cookies[:olaf] = :polaf
+      end
+    end
+    EOF
+    run_and_check_for_info(@runner, "app/controllers/application.rb", content, ":1 - visually check getting and setting cookies (sensitive information?)")
+  end
+  
+  it "should not warn of cookies in application controller" do
+    content = <<-EOF
+    class ApplicationController < ActionController::Base
+    end
+    EOF
+    run_and_check_for_no_info(@runner, "app/models/application.rb", content)
+  end
+
+
+  it "should warn of cookies in any other controller" do
+    content = <<-EOF
+    class FooController < ApplicationController
+      def bar
+        cookies[:olaf] = :polaf
+      end
+    end
+    EOF
+    run_and_check_for_info(@runner, "app/controllers/foo_controller.rb", content, ":1 - visually check getting and setting cookies (sensitive information?)")
+  end
+  
+  it "should not warn of cookies in any other controller" do
+    content = <<-EOF
+    class Foo < ActiveRecord::Base
+    end
+    EOF
+    run_and_check_for_no_info(@runner, "app/models/foo_controller.rb", content)
+  end
+
+
+  it "should warn of cookies in helpers" do
+    content = <<-EOF
+    module FooHelper
+      def bar
+        cookies[:olaf] = :polaf
+      end
+    end
+    EOF
+    run_and_check_for_info(@runner, "app/helpers/foo_helper.rb", content, ":1 - visually check getting and setting cookies (sensitive information?)")
+  end
+  
+  it "should not warn of cookies in helpers" do
+    content = <<-EOF
+    module FooHelper
+    end
+    EOF
+    run_and_check_for_no_info(@runner, "app/helpers/foo_helper.rb", content)
+  end
+  
+end
